7a6e2ebf7c372b2286e03295d21e51031bf9cb08
dnagyeelek <dnagyelek@gmail.com>
2018-03-05 14:15:23 +0200

    added skeleton directories

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,48 @@ require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 var gulp = require('gulp'),
 		browserSync = require('browser-sync'),
+		sass = require('gulp-sass'),
+		postcss = require('gulp-postcss'),
+		autoprefixer = require('autoprefixer'),
+		//		cssnano = require('cssnano'),
+		csso = require('postcss-csso'),
+//		csswring = require('csswring'),
+		plumber = require('gulp-plumber'),
+//		notify = require("gulp-notify"),
 		// your-expected-site-name
 		watch_path = './wordpress/wp-content/',
 		// change site-theme
-		browser_sync = watch_path + 'themes/';
+		browser_sync = watch_path + 'themes/pendock/';
 
 
-gulp.task('default', function () {
+gulp.task('serve', ['sass'], function () {
 
-	init_watch(false, 3001, {target: 'http://docker.dev'});
-	// init_watch(true, 3001, {target: 'https://docker.dev'});
-});
-
-function init_watch(https, port, proxy) {
-
-	var defaults = {
-		port: 3001
-	};
-	if (typeof https !== 'boolean') {
-		https = defaults.https;
-	}
-	if (typeof port !== 'number') {
-		port = defaults.port;
-	}
-	if (typeof proxy === 'string') {
-		proxy = {target: proxy};
-	} // fix the format if only target is passed
-	if (typeof proxy !== 'object') {
-		proxy = defaults.proxy;
-	}
-
-	browserSync({
-		proxy: proxy,
-		https: https,
-		port: port,
+	browserSync.init({
+		proxy: "http://docker.dev",
 		notify: false,
 		open: false, // no new browser tab
-		snippetOptions: {
-			whitelist: ['/wp-admin/admin-ajax.php'],
-			blacklist: ['/wp-admin/**']
-		}
 	});
 
-	gulp.watch([browser_sync + '/**/*',
-		// no babysit for minified nor scss
-		'!' + browser_sync + '/**/*.min.css',
-		'!' + browser_sync + '/**/*.scss'], function () {
-		browserSync.reload();
-	});
-}
+	gulp.watch(browser_sync + "/**/*.scss", ['sass']);
+	gulp.watch(browser_sync + "/**/*.php").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+	var plugins = [
+		autoprefixer(),
+//		cssnano(), // good
+		csso(),    // old best
+//		csswring(), // new best
+	];
+	return gulp.src(browser_sync + "/**/*.scss", {base: browser_sync})
+			.pipe(plumber())
+			//			.pipe(sass())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(postcss(plugins))
+			.pipe(gulp.dest(function (file) {
+				return file.base;
+			}))
+			.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
